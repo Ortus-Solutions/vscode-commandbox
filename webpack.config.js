e7c4fc88227548b8fcee2338650904224ca8d985
KamasamaK <KamasamaK@users.noreply.github.com>
2022-04-13 09:20:54 -0400

    Setup for release workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,46 @@
 // @ts-check
+/** @typedef {import('webpack').Configuration} WebpackConfig */
 
 "use strict";
 
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require( "path" );
+const { CleanWebpackPlugin } = require( "clean-webpack-plugin" );
 
-/** @type {import('webpack').Configuration} */
+/** @type WebpackConfig */
 const config = {
-	target: "node", // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+	target : "node", // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 
-	entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
-	output: {
+	entry  : "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+	output : {
 		// the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-		path: path.resolve(__dirname, "dist"),
-		filename: "extension.js",
-		libraryTarget: "commonjs2",
-		devtoolModuleFilenameTemplate: "../[resource-path]"
+		path                          : path.resolve( __dirname, "dist" ),
+		filename                      : "extension.js",
+		libraryTarget                 : "commonjs2",
+		devtoolModuleFilenameTemplate : "../[resource-path]"
 	},
-	devtool: "source-map",
-	plugins: [
+	devtool : "source-map",
+	plugins : [
 		new CleanWebpackPlugin()
 	],
-	externals: {
-		vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+	externals : {
+		vscode : "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
 	},
-	resolve: {
+	resolve : {
 		// support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-		extensions: [".ts", ".js"]
+		extensions : [ ".ts", ".js" ]
 	},
-	module: {
-		rules: [
+	module : {
+		rules : [
 			{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				use: [
+				test    : /\.ts$/,
+				exclude : /node_modules/,
+				use     : [
 					{
-						loader: "ts-loader",
-						options: {
-							compilerOptions: {
-								"module": "es6" // override `tsconfig.json` so that TypeScript emits native JavaScript modules.
+						loader  : "ts-loader",
+						options : {
+							compilerOptions : {
+								"module"           : "es6", // override `tsconfig.json` so that TypeScript emits native JavaScript modules.
+								"moduleResolution" : "node"
 							}
 						}
 					}
